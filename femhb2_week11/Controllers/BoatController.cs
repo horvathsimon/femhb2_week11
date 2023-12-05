b2df5b9da5580cc05b2f91964e324cbc8111cf42
horvathsimon <simon.horvath@stud.uni-corvinus.hu>
2023-12-05 08:54:34 +0100

    Feladat megoldás - part III

--- femhb2_week11/Controllers/BoatController.cs
+++ femhb2_week11/Controllers/BoatController.cs
@@ -17,5 +17,29 @@ namespace femhb2_week11.Controllers
 
             return Ok(kérdések);
         }
+
+        [HttpGet]
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
+        {
+            HajosContext context = new HajosContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
+        }
+
+        [HttpGet]
+        [Route("questions/count")]
+        public int M4() //Tetszőleges metódusnév
+        {
+            HajosContext context = new HajosContext();
+            int kérdésekSzáma = context.Questions.Count();
+
+            return kérdésekSzáma;
+        }
     }
 }
