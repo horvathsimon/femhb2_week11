e63a60aa8ee17d87ced1babe3e89a76fc736081d
horvathsimon <simon.horvath@stud.uni-corvinus.hu>
2023-12-05 08:25:58 +0100

    Feladat megoldás - part I

---
+++ femhb2_week11/Controllers/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using femhb2_week11.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace femhb2_week11.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
---
+++ femhb2_week11/Controllers/TesztController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace femhb2_week11.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            return Ok(szoveg.ToUpper());
+        }
+    }
+}
